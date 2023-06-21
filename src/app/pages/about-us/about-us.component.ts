8636c87bd6bf6d2c567de9a7e41cc8a5a2e11611
JAGDISH0008 <jagadeesh26597@gmail.com>
2023-06-21 10:05:20 -0700

    feat: added about us , contact page components.

---
+++ src/app/pages/about-us/about-us.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: "app-about-us-page",
+  templateUrl: "./about-us.component.html"
+})
+export class AboutusPageComponent {
+  constructor() {
+
+  }
+
+}
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PagesComponent } from './pages.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { AboutusPageComponent } from './about-us/about-us.component';
 
 const routes: Routes = [
   {
     path: '',
     component: PagesComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'contact-page',
+    component: ContactPageComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'about-us',
+    component: AboutusPageComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'pricing-plans',
+    component: AboutusPageComponent,
     pathMatch: 'full'
   },
 ];
